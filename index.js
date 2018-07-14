3df89f7a51108cd17f6cd264ec59d9bed5bfe2b1
Nathan Marianovsky <nathanmarianovsky@gmail.com>
2018-07-14 02:34:02 -0700

    Final Version
    Cleaned up the code and handled all possible outputs. Also throws a message in case some input is not proper.

--- index.js
+++ index.js
@@ -1,57 +1,61 @@
+// Declare the necessary variables
 var math = require("mathjs"),
 	fs = require("fs");
 
-
-
-
-
-
+// Performs a single iteration of the Bisection Method and returns the new array containing the endpoints and the new candidate.
 var bisect = (f, interval) => {
-	var middle = f((interval[0] + interval[1]) / 2),
+	var middle = (interval[0] + interval[1]) / 2,
+		eval1 = f(interval[0]),
+		eval2 = f(interval[1]),
+		value = f(middle),
 		holder = 0,
 		arr = [];
-	if(f(interval[0]) > 0 && f(interval[1]) < 0) {
-		if(middle <= 0) {
+	if(eval1 > 0 && eval2 < 0) {
+		if(value <= 0) {
 			holder = interval[1];
-			interval[1] = (interval[0] + interval[1]) / 2;
+			interval[1] = middle;
 		}
 		else {
 			holder = interval[0];
-			interval[0] = (interval[0] + interval[1]) / 2;
+			interval[0] = middle;
 		}
-		interval[2] = holder;
 	}
-	else if(f(interval[0]) < 0 && f(interval[1]) > 0) {
-		if(middle >= 0) {
+	else if(eval1 < 0 && eval2 > 0) {
+		if(value >= 0) {
 			holder = interval[1];
-			interval[1] = (interval[0] + interval[1]) / 2;
+			interval[1] = middle;
 		}
 		else {
 			holder = interval[0];
-			interval[0] = (interval[0] + interval[1]) / 2;
+			interval[0] = middle;
 		}
-		interval[2] = holder;
-		// f(middle) >= 0 ? interval[1] = middle : interval[0] = middle;
 	}
+	interval[2] = holder;
 	return interval;
 };
 
-// console.log(bisect(f, [1,2,0]));
-
+// Iterates through the Bisection Method until a desired value is found or breaks after one million steps.
 var main = (f, interval, error) => {
-	console.log(interval);
-	console.log(error);
 	if(f(interval[0]) == 0) { return interval[0]; }
 	if(f(interval[1]) == 0) { return interval[1]; }
+	var count = 0;
 	while(!(f(interval[0]) == 0 || f(interval[1]) == 0 || Math.abs(f(interval[2])) <= error)) {
-		console.log(interval);
+		if(count == 10000) { console.log("After ten thousand steps " +
+			"there is still no value of x that satisfies the condition with the given error.") }
+		else if(count == 1000000) { 
+			console.log("After one million steps there is still no value of x that satisfies " + 
+				"the condition and this is enough to convince us that there might not be such a value " + 
+				"or the interval was just not tight enough!");
+			interval[2] = "UNDEFINED";
+			break;
+		}
 		interval = bisect(f, interval);
+		count++;
 	}
 	return interval[2];
 };
 
-// main(f, [1, 2, 0], 1 / 10);
-
+// Read and parse the input. Call the main function and output the result if there is one. 
 fs.readFile("input.txt", "utf8", (err, data) => {
 	if(err) { throw err; }
 	var rows = data.split("\n").map(elem => elem.split("\r")[0]),
@@ -59,7 +63,15 @@ fs.readFile("input.txt", "utf8", (err, data) => {
 		right = parseFloat(rows[3].split(",")[1].split("]")[0]),
 		control = parseFloat(rows[5]),
 		parser = new math.parser();
-	parser.eval(rows[1]);
-	var func = parser.get("f");
-	console.log("The first value of x that makes |f(x)| < " + control + " is " + main(func, [left, right, 0], control) + ".");
+	try {
+		parser.eval(rows[1]);
+		var func = parser.get("f");
+		var result = main(func, [left, right, 0], control);
+		if(result != "UNDEFINED") {
+			console.log("The first value of x inside " + rows[3].replace(/ /g,'') + " that makes |f(x)| < " + control + " is " + result.toPrecision(15));
+		}
+	}
+	catch(prob) {
+		console.log("There was a problem parsing either the function, interval, or error you provided!");	
+	}
 });
