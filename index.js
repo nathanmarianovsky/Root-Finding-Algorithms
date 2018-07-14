2b4a89b91887411415cfed8a81df07fb4a4b18ee
Nathan Marianovsky <nathanmarianovsky@gmail.com>
2018-07-14 01:00:05 -0700

    Reading a File Input

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-var math = require("mathjs");
+var math = require("mathjs"),
+	fs = require("fs");
 
-var parser = new math.parser();
 
-parser.eval("f(x) = (x^2)-2");
-var f = parser.get("f");
 
-// console.log(f(Math.PI,1));
 
 
 
@@ -23,7 +20,6 @@ var bisect = (f, interval) => {
 			interval[0] = (interval[0] + interval[1]) / 2;
 		}
 		interval[2] = holder;
-		// f(middle) <= 0 ? interval[1] = middle : interval[0] = middle;
 	}
 	else if(f(interval[0]) < 0 && f(interval[1]) > 0) {
 		if(middle >= 0) {
@@ -44,16 +40,26 @@ var bisect = (f, interval) => {
 
 var main = (f, interval, error) => {
 	console.log(interval);
+	console.log(error);
 	if(f(interval[0]) == 0) { return interval[0]; }
 	if(f(interval[1]) == 0) { return interval[1]; }
-	console.log(f(interval[0]));
-	console.log(f(interval[1]));
-	console.log(f(interval[2]));
 	while(!(f(interval[0]) == 0 || f(interval[1]) == 0 || Math.abs(f(interval[2])) <= error)) {
 		console.log(interval);
 		interval = bisect(f, interval);
 	}
-	console.log(interval);
+	return interval[2];
 };
 
-main(f, [1, 2, 0], 1 / 10);
+// main(f, [1, 2, 0], 1 / 10);
+
+fs.readFile("input.txt", "utf8", (err, data) => {
+	if(err) { throw err; }
+	var rows = data.split("\n").map(elem => elem.split("\r")[0]),
+		left = parseFloat(rows[3].split(",")[0].split("[")[1]),
+		right = parseFloat(rows[3].split(",")[1].split("]")[0]),
+		control = parseFloat(rows[5]),
+		parser = new math.parser();
+	parser.eval(rows[1]);
+	var func = parser.get("f");
+	console.log("The first value of x that makes |f(x)| < " + control + " is " + main(func, [left, right, 0], control) + ".");
+});
